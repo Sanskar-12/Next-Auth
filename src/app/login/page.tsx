e85a2d9b8c971138d37aeb6082dd836aef48b6aa
Sanskar-12 <sanskarv2004@gmail.com>
2024-04-17 17:51:17 +0530

    seventh commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const Login = () => {
-  return <div>Login</div>;
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const loginHandler = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await axios.post("/api/users/login", user);
+
+      toast.success(data.message);
+      setLoading(false);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log(error);
+      setLoading(false);
+      toast.error(error.response.data.error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing..." : "Login"}</h1>
+      <hr />
+
+      <label htmlFor="email">Email</label>
+      <input
+        type="text"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="text"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={loginHandler}
+        disabled={loading}
+      >
+        Login
+      </button>
+      <Link href={"/signup"}>Visit Sign Up Page</Link>
+    </div>
+  );
 };
 
 export default Login;
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const VerifyEmail = () => {
-  return <div></div>;
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyEmailHandler = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+      setError(false);
+    } catch (error: any) {
+      setError(error);
+      console.log(error.response.data.error);
+    }
+  };
+
+  useEffect(() => {
+    setError(false);
+    const urlToken = window.location.search.split("=")[1];
+    setToken(urlToken || "");
+  }, []);
+
+  useEffect(() => {
+    setError(false);
+    if (token.length > 0) {
+      verifyEmailHandler();
+    }
+  }, [token]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl">Verify Email</h1>
+      <h2 className="p-2 bg-orange-500 text-black">
+        {token ? `${token}` : "No Token"}
+      </h2>
+      {verified && (
+        <div>
+          <h2>Verified</h2>
+          <Link href={"/login"}>Login</Link>
+        </div>
+      )}
+      {error && (
+        <div>
+          <h2>Error</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default VerifyEmail;
